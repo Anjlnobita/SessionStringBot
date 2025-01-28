10dc3b3593d04d9b97ce871559b1b4dc68e5f4cd
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-28 20:22:57 +0530

    Update sql.py

--- ssnbot/db/sql.py
+++ ssnbot/db/sql.py
@@ -1,71 +1,35 @@
 import threading
-from sqlalchemy import create_engine
-from sqlalchemy import Column, TEXT, BigInteger
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.pool import StaticPool
-from ssnbot import DB_URL
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['your_database_name']
+collection = db['broadcast']
 
-BASE = declarative_base()
-
-
-class Broadcast(BASE):
-    __tablename__ = "broadcast"
-    user_id = Column(BigInteger, primary_key=True)
-    user_name = Column(TEXT)
-
-    def __init__(self, user_id, user_name):
-        self.user_id = user_id
-        self.user_name = user_name
-
-
-def start() -> scoped_session:
-    engine = create_engine(
-        DB_URL, client_encoding="utf8", poolclass=StaticPool)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-SESSION = start()
 INSERTION_LOCK = threading.RLock()
 
 
 async def add_user(user_id, user_name):
     with INSERTION_LOCK:
-        try:
-            usr = SESSION.query(Broadcast).filter_by(user_id=user_id).one()
-        except NoResultFound:
-            usr = Broadcast(user_id=user_id, user_name=user_name)
-            SESSION.add(usr)
-            SESSION.commit()
+        if collection.find_one({"user_id": user_id}) is None:
+            usr = {"user_id": user_id, "user_name": user_name}
+            collection.insert_one(usr)
 
 
 async def is_user(user_id):
     with INSERTION_LOCK:
-        try:
-            usr = SESSION.query(Broadcast).filter_by(user_id=user_id).one()
-            return usr.user_id
-        except NoResultFound:
-            return False
+        usr = collection.find_one({"user_id": user_id})
+        return usr['user_id'] if usr else False
 
 
 async def query_msg():
     try:
-        query = SESSION.query(Broadcast.user_id).order_by(Broadcast.user_id)
-        return query.all()
+        query = collection.find({}, {"_id": 0, "user_id": 1}).sort("user_id")
+        return list(query)
     finally:
-        SESSION.close()
+        client.close()
 
 
 async def del_user(user_id):
     with INSERTION_LOCK:
-        try:
-            usr = SESSION.query(Broadcast).filter_by(user_id=user_id).one()
-            SESSION.delete(usr)
-            SESSION.commit()
-        except NoResultFound:
-            pass
-        
+        collection.delete_one({"user_id": user_id})

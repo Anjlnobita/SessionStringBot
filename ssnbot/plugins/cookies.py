5ef34f5c6c7731e1811b27fd2d5a19cae3aa7668
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-28 23:02:28 +0530

    Update cookies.py

--- ssnbot/plugins/cookies.py
+++ ssnbot/plugins/cookies.py
@@ -27,3 +27,28 @@ async def send_cookies(client, message):
 @app.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text("Welcome to the Cookies Generator Bot!")
+
+
+
+@app.on_message(filters.command("authtoken") & SUDOERS)
+async def auth_token_status(client, message):
+    status_message = "**Auth Token Status:**\nChecking..."
+    status_msg = await message.reply_text(status_message)
+
+    use_token = await check_auth_token()
+    status_message = "**Auth Token Status:**\n"
+    status_message += "✅ Alive" if use_token else "❌ Dead"
+    await status_msg.edit_text(status_message)
+
+    if not use_token:
+        status_message += "\n\n**Generating a new Auth token...**"
+        await status_msg.edit_text(status_message)
+        try:
+            os.system(
+                f"yt-dlp --username oauth2 --password '' -F https://www.youtube.com/watch?v=LLF3GMfNEYU"
+            )
+            await message.reply_text("**✅ Successfully generated a new token.**")
+        except Exception as ex:
+            await message.reply_text(
+                f"**❌ Failed to generate a new token: {str(ex)}**"
+            )
